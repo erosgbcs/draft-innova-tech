4bf063ddfb7cc6c780513c69eb3e77fa33cd9e78
PC User <PC User@DESKTOP-I5NI2E6>
2026-03-19 16:30:54 +0800

    uyhii

--- frmLoading.vb
+++ frmLoading.vb
@@ -4,57 +4,14 @@
         Timer1.Interval = 1000 ' Speed of progress
         Timer1.Start()
         lblPercent.Text = "0%"
-        Progressbar1.Value = 0
+        ProgressBar1.Value = 0
     End Sub
 
     ' Add "Handles Timer1.Tick" to the end of this line!
     Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
-        If Progressbar1.Value < 100 Then
-            Progressbar1.Value += 2
-            lblPercent.Text = Progressbar1.Value & "%........Initializing "
-        Else
-            Timer1.Stop() ' Good practice to use parentheses
-
-            ' Open Login Form
-            FrmLogin.Show() ' Use the default instance or your Dim
-
-            ' Hide the loading screen
-            Me.Hide()
-        End If
-    End Sub
-
-
-    Private Sub ProgressBar1_Click(sender As Object, e As EventArgs)
-    End Sub
-    ' Add this inside your main form's code
-    Private Sub Form_FormClosing(sender As Object, e As FormClosingEventArgs) Handles MyBase.FormClosing
-        ' This kills the hidden background process completely
-        Application.Exit()
-    End Sub
-
-
-    Private Sub Panel1_Paint(sender As Object, e As PaintEventArgs) Handles Panel1.Paint
-
-    End Sub
-
-    Private Sub lblPercent_Click(sender As Object, e As EventArgs) Handles lblPercent.Click
-
-    End Sub
-End Class
-Public Class FrmLoading
-    ' Use Capital 'F' for the naming rule
-    Private Sub FrmLoading_Load(sender As Object, e As EventArgs) Handles MyBase.Load
-        Timer1.Interval = 1000 ' Speed of progress
-        Timer1.Start()
-        lblPercent.Text = "0%"
-        Progressbar1.Value = 0
-    End Sub
-
-    ' Add "Handles Timer1.Tick" to the end of this line!
-    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
-        If Progressbar1.Value < 100 Then
-            Progressbar1.Value += 2
-            lblPercent.Text = Progressbar1.Value & "%........Initializing Database"
+        If ProgressBar1.Value < 100 Then
+            ProgressBar1.Value += 2
+            lblPercent.Text = ProgressBar1.Value & "%........Initializing"
         Else
             Timer1.Stop() ' Good practice to use parentheses
 
